7b452dd0155966592736dddd49d2e1ac46872a6c
Hardik210 <72506941+Hardik210@users.noreply.github.com>
2026-04-17 13:28:22 +0530

    Add files via upload
    firts commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const position = section.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if (position < screenPosition) {
+            section.style.opacity = 1;
+            section.style.transform = 'translateY(0)';
+        }
+    });
+});
+
+sections.forEach(section => {
+    section.style.opacity = 0;
+    section.style.transform = 'translateY(50px)';
+    section.style.transition = '0.6s ease';
+});
